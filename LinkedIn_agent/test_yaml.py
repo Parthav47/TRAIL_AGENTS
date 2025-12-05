0a9ba8d69f43482f0631e6eda4d7013b63c8df6c
Parthav47 <parthavudayasankar@gmail.com>
2025-12-05 06:28:07 +0530

    Initial commit

---
+++ LinkedIn_agent/test_yaml.py
@@ -0,0 +1,57 @@
+from crewai import Agent, Task, Crew, Process, LLM
+from crewai.project import CrewBase, agent, task, crew
+from crewai_tools import TavilySearchTool, ScrapeWebsiteTool, DirectoryReadTool
+
+from dotenv import load_dotenv
+load_dotenv()
+
+@CrewBase
+class BlogCrew():
+
+    agents_config="config/agent.yaml"
+    tasks_config="config/task.yaml"
+
+    @agent
+    def researcher(self)->Agent:
+        return Agent(
+            config=self.agents_config['research_agent'],
+            tools=[ TavilySearchTool(), ScrapeWebsiteTool() ],
+            verbose =True
+        )
+    
+    @agent
+    def writer(self)->Agent:
+        return Agent(
+            config=self.agents_config['writer_agent'],
+            tools=[ DirectoryReadTool() ],
+            verbose =True
+        )
+    
+    @task
+    def research_task(self)->Task:
+        return Task(
+            config=self.tasks_config["researcher_task"],
+            Agent=self.researcher(),
+            verbose=True
+        )
+    
+    @task
+    def blog_task(self)->Task:
+        return Task(
+            config=self.task_config["blog_task"],
+            Agent=self.writer(),
+            verbose=True
+        )
+    @crew
+    def crew(self)->Crew:
+        return Crew(
+            agents=[self.researcher(), self.writer()],
+            tasks=[self.research_task(), self.blog_task]
+        )
+
+
+if __name__ == "__main__":
+    blog_crew=BlogCrew()
+    #result=blog_crew.crew().kickoff(inputs={"topic":"Impact of AI on Modern Education"})
+    #print(result)
+    print(blog_crew.agents_config)
